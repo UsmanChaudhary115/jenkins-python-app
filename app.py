94c1d63bc9ff4e219fcaa23ecf3d216ca3a74090
UsmanChaudhary115 <oosmanaliii9@gmail.com>
2025-11-12 04:00:08 +0500

    update logic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 def main():
     print("Hello from Jenkins CI/CD with Python!")
+    print("This is a simple Python application running in a CI/CD pipeline.")
+    print("Build and deployment successful.")
 
 if __name__ == "__main__":
     main()
